89313ec66ae5b7734ed8269475d920a208249618
Sultonxon <91827090+Sultonxon@users.noreply.github.com>
2023-01-24 13:46:49 +0500

    Back modified

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,14 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:SqlServer"]);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { SocialAuthService, GoogleLoginProvider, SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'front';
+
+  user: SocialUser | null;
+
+  constructor(private authService: SocialAuthService,@Inject("SocialAuthServiceConfig") private signInOptions:any) {
+    this.user = null;
+    console.log(this.signInOptions);
+
+    this.authService.authState.subscribe((user: SocialUser) => {
+      console.log(user);
+      this.user = user;
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID, this.signInOptions).then((x:any) => { console.log(x);} );
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
 }
