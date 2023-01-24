c09a2161cf5f05ecd70d5790111b86f8ed2acfee
Sultonxon <91827090+Sultonxon@users.noreply.github.com>
2023-01-24 16:34:06 +0500

    Completed

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using backend.Services;
+using Google.Apis.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace backend.Controllers
 {
@@ -7,6 +11,29 @@ namespace backend.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        public class AuthenticateRequest
+        {
+            [Required]
+            public string IdToken { get; set; }
+        }
 
+        private readonly JwtGenerator _jwtGenerator;
+
+        public UserController(IConfiguration configuration)
+        {
+            _jwtGenerator = new JwtGenerator(configuration["JwtPrivateSigningKey"]);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] AuthenticateRequest data)
+        {
+            GoogleJsonWebSignature.ValidationSettings settings = new GoogleJsonWebSignature.ValidationSettings();
+            settings.Audience = new List<string> { "950688783434-unpse286sph97v9mvjo1djgmn6k5ajpb.apps.googleusercontent.com" };
+            GoogleJsonWebSignature.Payload payload = GoogleJsonWebSignature.ValidateAsync(data.IdToken, settings).Result;
+
+            return Ok(new { AuthToken = _jwtGenerator.CreateUserAuthToken(payload.Email)});
+
+        }
     }
 }
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { SocialAuthService, GoogleLoginProvider, SocialUser } from '@abacritt/angularx-social-login';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,19 @@ export class AppComponent {
 
   user: SocialUser | null;
 
-  constructor(private authService: SocialAuthService,@Inject("SocialAuthServiceConfig") private signInOptions:any) {
+  constructor(private authService: SocialAuthService,@Inject("SocialAuthServiceConfig") private signInOptions:any, private http: HttpClient) {
     this.user = null;
     console.log(this.signInOptions);
 
     this.authService.authState.subscribe((user: SocialUser) => {
       console.log(user);
+      if(user){
+        http.post<any>("https://localhost:5001/User/authenticate",{idToken:user.idToken})
+          .subscribe((authToken:any) => {
+            console.log(authToken.authToken);
+            localStorage.setItem("authToken",authToken);
+          });
+      }
       this.user = user;
     });
   }
